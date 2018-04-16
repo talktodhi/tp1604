04679d8dbe54dddc5b5d76658bc62e474da6f703
talktodhi <talktodhi@gmail.com>
2018-04-16 13:36:35 +0530

    Changes

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Hello";
+$fp = fsockopen('127.0.0.1', 25, $errno, $errstr, 5);
+if (!$fp) {
+    // port is closed or blocked
+	echo "Blocked";
+} else {
+    // port is open and available
+	echo "Not Blocked";
+    fclose($fp);
+}
